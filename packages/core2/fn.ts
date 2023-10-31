0a3eae3dd3e35d53e071a4ede210e4466acc432d
npmstudy <npmstudy@qq.com>
2023-10-31 13:51:41 +0800

    feat: stash

--- packages/core2/fn.ts
+++ packages/core2/fn.ts
@@ -1,14 +1,14 @@
 import { F } from 'vitest/dist/reporters-5f784f42';
 
 export class Fn {
-  public name: string;
-  public init: Array<Function>;
-  public load: Array<Function>;
+  // public name: string;
+  // public init: Array<Function>;
+  // public load: Array<Function>;
 
   constructor() {
-    this.name = 'fn';
-    this.init = [];
-    this.load = [];
+    // this.name = 'fn';
+    // this.init = [];
+    // this.load = [];
   }
 
   async render(ctx, next) {
