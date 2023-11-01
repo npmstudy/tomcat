bb66245faa17aed88319ebe75c8aaaea3b21cb5f
npmstudy <npmstudy@qq.com>
2023-11-01 13:49:18 +0800

    chore: refact

--- packages/core2/src/fn.ts
+++ packages/core2/src/fn.ts
@@ -1,4 +1,10 @@
+import debug from 'debug';
+
 import Plugable from './base';
+import { mountMiddleware } from './core';
+import { isArrowFunction, getHttpMethods } from './utils';
+const log = debug('@tomrpc/core');
+
 export default class Fn extends Plugable {
   // public name: string;
 
@@ -16,20 +22,68 @@ export default class Fn extends Plugable {
 
     const c = this.c();
     // this.addLoad(c);
-    this.server['fn'].add = function (i) {
+
+    this.config['server']['fn'] = function (items) {
       if (!this.config['functions']) this.config['functions'] = [];
-      this.config['functions'].push(i);
+      if (Object.entries(items)) {
+        for (const [name, fn] of Object.entries(items)) {
+          if (isArrowFunction(fn)) {
+            console.log(
+              `this.rpcFunctions[${name}] is arrow function, please use ctx as param, not this`
+            );
+          }
+          if (this.rpcFunctions[name]) {
+            log(`add ${name}: ${fn}`);
+            console.log(`this.rpcFunctions[${name}] exisit`);
+          }
+
+          this.rpcFunctions[name] = fn;
+        }
+      } else {
+        this.config['functions'].push(items);
+      }
     };
   }
   process() {
+    return mountMiddleware(this.config['functions']);
+  }
+
+  pre() {
     return async (ctx, next) => {
-      console.dir('process child');
-      if (ctx.path === '/') {
-        ctx.body = { '/': 23 };
-      } else {
-        // ctx.body = { '2323api': 23 };
+      log('beforeOne');
+      const key = ctx.path.replace('/', '').split('/').join('.');
+      // this.config.beforeOne(ctx, key);
+
+      const lastKey = key.split('.').pop();
+      const httpMethods = getHttpMethods();
+
+      const supportMethods = [];
+      httpMethods.forEach(function (m) {
+        if (lastKey.indexOf(m) != -1) {
+          log(m);
+          supportMethods.push(m);
+          return m;
+        }
+      });
+      // console.log(supportMethods);
+
+      if (supportMethods.length === 0) {
+        log('没有匹配到包含get/post等方法的函数');
         await next();
+      } else if (ctx.method === supportMethods[0]) {
+        log('匹配到包含get/post等方法的函数');
+        await next();
+      } else {
+        log('匹配到包含get/post等方法的函数，但method不对');
+        ctx.body =
+          'process fn:' +
+          lastKey +
+          ' , you need send ' +
+          supportMethods[0] +
+          ' request from client';
       }
+
+      log('beforeOne end');
     };
   }
 
