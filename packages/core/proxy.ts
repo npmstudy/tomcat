7a89b428fdd7d564bda89bc0d761193fc704f233
npmstudy <npmstudy@qq.com>
2023-11-27 21:19:00 +0800

    chore: stash

--- packages/core/proxy.ts
+++ packages/core/proxy.ts
@@ -1,9 +1,24 @@
 import { Proxy } from './src/proxy';
-export default class TestProxy extends Proxy {
+export class TestProxy extends Proxy {
   constructor(cfg?) {
     super(cfg);
 
     this.config.proxy.inject = 'init';
+    this.config.proxy.before = ['fn'];
+  }
+  proxy() {
+    return async (ctx, next) => {
+      console.dir('TestProxy process child' + ctx.path);
+      await next();
+    };
+  }
+}
+
+export class TestProxy2 extends Proxy {
+  constructor(cfg?) {
+    super(cfg);
+
+    this.config.proxy.inject = 'before';
     this.config.proxy.before = ['fn'];
   }
   proxy() {
