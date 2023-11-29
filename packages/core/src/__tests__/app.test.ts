45e8df613c02f6603486249cf433afee50026ff4
npmstudy <npmstudy@qq.com>
2023-11-29 19:54:49 +0800

    chore: refact

--- packages/core/src/__tests__/app.test.ts
+++ packages/core/src/__tests__/app.test.ts
@@ -24,6 +24,14 @@ describe('app', async () => {
     }
   });
 
+  rpc.fn('com.yourcompony.a', function (a: string) {
+    return { a: a };
+  });
+
+  rpc.fn('com.yourcompony.b', function (a: string) {
+    return `${this.path} , ${a} `;
+  });
+
   const request = supertest(rpc.callback());
 
   it('should acess /a', async () => {
@@ -46,5 +54,12 @@ describe('app', async () => {
     expect(res.type).toEqual('application/json');
     expect(res.status).toEqual(200);
     expect(res.body).toEqual({ a: 'hello', method: 'post' });
+  });
+
+  it('should acess com.yourcompony.b', async () => {
+    const res = await request.get('/api/com/yourcompony/b?$p=["hello"]');
+    expect(res.type).toEqual('text/plain');
+    expect(res.status).toEqual(200);
+    expect(res.text).toEqual('/com/yourcompony/b , hello ');
   });
 });
