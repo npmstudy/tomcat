b173034e42ee190929308b3a83476f89648cacea
npmstudy <npmstudy@qq.com>
2023-11-29 23:05:47 +0800

    chore: refact

--- packages/core/src/__tests__/proxy.test.ts
+++ packages/core/src/__tests__/proxy.test.ts
@@ -1,8 +1,9 @@
+import debug from 'debug';
 import supertest from 'supertest';
 import { describe, expect, it, vi } from 'vitest';
 
 import { TestProxy, TestProxy2 } from '../../proxy';
-import { createServer } from '../index';
+import { createServer, Proxy } from '../index';
 
 describe('proxy', async () => {
   it('should enable proxy1', async () => {
@@ -55,5 +56,21 @@ describe('proxy', async () => {
     expect(res.body).toEqual({ a: 'hello' });
 
     expect(spy).toHaveBeenCalled();
+  });
+
+  it('should enable proxy2', async () => {
+    const tp = new Proxy({});
+    tp.app.use(tp.proxy());
+
+    tp.app.use(async (ctx) => {
+      ctx.body = { a: 'a' };
+    });
+
+    const request = supertest(tp.app.callback());
+
+    const res = await request.get('/');
+    expect(res.type).toEqual('application/json');
+    expect(res.status).toEqual(200);
+    expect(res.body).toEqual({ a: 'a' });
   });
 });
